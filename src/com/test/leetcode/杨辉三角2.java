fa112eb5789de033b7c99e87fe5466f23c5aa5a0
yy598162719 <598162719@qq.com>
2019-11-12 22:41:08 +0800

    111

---
+++ src/com/test/leetcode/杨辉三角2.java
@@ -0,0 +1,40 @@
+package com.test.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//119
+public class 杨辉三角2 {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> pre = new ArrayList<>();
+        List<Integer> cur = new ArrayList<>();
+        for (int i = 0; i <= rowIndex; i++) {
+            cur = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    cur.add(1);
+                } else {
+                    cur.add(pre.get(j - 1) + pre.get(j));
+                }
+            }
+            pre = cur;
+        }
+        return cur;
+    }
+    public static List<Integer> getRow1(int rowIndex) {
+        int pre = 1;
+        List<Integer> cur = new ArrayList<>();
+        cur.add(1);
+        for (int i = 1; i <= rowIndex; i++) {
+            for (int j = i - 1; j > 0; j--) {
+                cur.set(j, cur.get(j - 1) + cur.get(j));
+            }
+            cur.add(1);//补上每层的最后一个 1
+        }
+        return cur;
+    }
+
+    public static void main(String[] args) {
+        getRow1(3);
+    }
+}
